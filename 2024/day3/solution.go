8caa7c5131f1f51ea7ae8df2c1a38ad33a43d1ab
Beyram <beyram.taglietti@gmail.com>
2024-12-03 15:21:00 +0100

    2024 day3 part2 enhancement

--- 2024/day3/solution.go
+++ 2024/day3/solution.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"regexp"
-	"slices"
 	"strings"
 )
 
@@ -43,113 +42,27 @@ func SolveP2() {
 
 	line := fullPuzzle.String()
 
-	matchesMap := make(matchMap)
-	matchesMap.findMultiplications(line)
-	matchesMap.findCmds(line)
-
-	fmt.Println("total sum with rules:", multiplyWithRules(matchesMap))
-}
-
-type command string
-
-const (
-	do   command = "do"
-	dont command = "don't"
-)
-
-type matchFound struct {
-	isCmd                bool
-	cmd                  command
-	multiplicationResult int
-}
-
-type matchMap map[int]matchFound
-
-func (m *matchMap) findMultiplications(line string) {
-	pattern := `mul\(\d+,\d+\)`
-	re, err := regexp.Compile(pattern)
-	if err != nil {
-		log.Fatalf("Error compiling regex: %s", err.Error())
-
-	}
-
-	matches := re.FindAllString(line, -1)
-	matchesIdxs := re.FindAllStringIndex(line, -1)
-
-	for idx, match := range matches {
-		(*m)[matchesIdxs[idx][0]] = matchFound{
-			isCmd:                false,
-			multiplicationResult: multiply(match),
-		}
-	}
-
-}
-
-func (m *matchMap) findCmds(line string) {
-	pattern := `do`
-	re, err := regexp.Compile(pattern)
-	if err != nil {
-		log.Fatalf("Error compiling regex: %s", err.Error())
-	}
-
-	matches := re.FindAllString(line, -1)
-	matchesIdxs := re.FindAllStringIndex(line, -1)
-
-	for idx := range matches {
-		matchesAtIdx := matchesIdxs[idx][0]
-		fullWord := line[matchesAtIdx : matchesAtIdx+5]
-
-		if fullWord == string(dont) {
-			(*m)[matchesAtIdx] = matchFound{
-				isCmd: true,
-				cmd:   dont,
-			}
-		} else {
-			(*m)[matchesAtIdx] = matchFound{
-				isCmd: true,
-				cmd:   do,
-			}
-		}
-	}
-
-}
-
-func multiplyWithRules(matchesMap matchMap) (result int) {
-
-	keys := make([]int, len(matchesMap), len(matchesMap))
-
-	i := 0
-	for k := range matchesMap {
-		keys[i] = k
-		i++
-	}
-
-	slices.Sort(keys)
+	pattern := `mul\((\d+,\d+)\)|do\(\)|don't\(\)`
+	re := regexp.MustCompile(pattern)
+	operations := re.FindAllString(line, -1)
 
 	sum := 0
-	can := true
-	for _, key := range keys {
-
-		match := matchesMap[key]
-
-		switch match.isCmd {
-		case true:
-			switch match.cmd {
-			case do:
-				can = true
-			case dont:
-				can = false
-			}
-
-		case false:
-			if can {
-				sum += match.multiplicationResult
-			}
+	enabled := true
+	for _, v := range operations {
+		if v == "don't()" {
+			enabled = false
+			continue
 		}
-
+		if v == "do()" {
+			enabled = true
+			continue
+		}
+		if enabled {
+			sum += multiply(v)
+		}
 	}
 
-	return sum
+	fmt.Println("total sum with rules:", sum)
 }
 
 func multiply(op string) int {
